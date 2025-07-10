2d5517953afab6d662531a00d3f66fff616537eb
Ali <alitaghi8305.com>
2025-07-10 13:36:53 +0330

    game server season 3

--- season3/game_server/main.go
+++ season3/game_server/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"sync"
 )
 
+type Message struct {
+	sender string
+	text   string
+}
+
 type Player struct {
 	name         string
+	lowerName    string
 	currentMapId int
+	pointerToMap *Map
 	receiveChan  chan string
 	game         *Game
 	lock         sync.Mutex
@@ -15,7 +24,7 @@ type Player struct {
 
 type Map struct {
 	id          int
-	mapChatChan chan string
+	mapChatChan chan Message
 	players     map[string]*Player
 	lock        sync.RWMutex
 }
@@ -27,51 +36,159 @@ type Game struct {
 }
 
 func NewGame(mapIds []int) (*Game, error) {
-	m := make(map[int]*Map)
+	maps := make(map[int]*Map)
 	for _, v := range mapIds {
 		if v <= 0 {
-			return nil, errors.New("")
+			return nil, errors.New("invalid mapId, must be > 0")
 		}
-		m[v] = &Map{id: v}
+		maps[v] = &Map{
+			id:          v,
+			mapChatChan: make(chan Message, 100),
+			players:     make(map[string]*Player),
+		}
+		go maps[v].FanOutMessages()
 	}
-	myNewGame := &Game{maps: m}
-	return myNewGame, nil
+	return &Game{
+		maps:    maps,
+		players: make(map[string]*Player),
+	}, nil
 }
 
 func (g *Game) ConnectPlayer(name string) error {
-	val, ok := g.players[name]
-	if ok {
-		return errors.New(val.name)
-	} else {
-		newPlayer := &Player{name: name, game: g}
-		g.players[name] = newPlayer
-		return nil
+	lowerName := strings.ToLower(name)
+	g.lock.Lock()
+	defer g.lock.Unlock()
+
+	if _, ok := g.players[lowerName]; ok {
+		return errors.New("player already exists")
 	}
+	receiveChan := make(chan string, 100)
+
+	player := &Player{
+		name:         formatName(name),
+		lowerName:    lowerName,
+		receiveChan:  receiveChan,
+		game:         g,
+		currentMapId: 0,
+		pointerToMap: nil,
+	}
+	g.players[lowerName] = player
+	return nil
 }
 
 func (g *Game) SwitchPlayerMap(name string, mapId int) error {
+	lowerName := strings.ToLower(name)
+
+	g.lock.Lock()
+	defer g.lock.Unlock()
+
+	player, ok := g.players[lowerName]
+	if !ok {
+		return errors.New("player not found")
+	}
+	if player.currentMapId == mapId {
+		return errors.New("player already in this map")
+	}
+
+	newMap, ok := g.maps[mapId]
+	if !ok {
+		return errors.New("map not found")
+	}
+
+	if player.pointerToMap != nil {
+		player.pointerToMap.lock.Lock()
+		delete(player.pointerToMap.players, lowerName)
+		player.pointerToMap.lock.Unlock()
+	}
+
+	newMap.lock.Lock()
+	newMap.players[lowerName] = player
+	newMap.lock.Unlock()
+
+	player.lock.Lock()
+	player.currentMapId = mapId
+	player.pointerToMap = newMap
+	player.lock.Unlock()
+
 	return nil
 }
 
 func (g *Game) GetPlayer(name string) (*Player, error) {
-	return nil, nil
+	lowerName := strings.ToLower(name)
+
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+
+	player, ok := g.players[lowerName]
+	if !ok {
+		return nil, errors.New("player not found")
+	}
+	return player, nil
 }
 
 func (g *Game) GetMap(mapId int) (*Map, error) {
-	return nil, nil
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+
+	m, ok := g.maps[mapId]
+	if !ok {
+		return nil, errors.New("map not found")
+	}
+	return m, nil
 }
 
 func (m *Map) FanOutMessages() {
+	for {
+		msg := <-m.mapChatChan
+
+		m.lock.RLock()
+		for key, player := range m.players {
+			if key == msg.sender {
+				continue
+			}
+			select {
+			case player.receiveChan <- msg.text:
+			default:
+
+			}
+		}
+		m.lock.RUnlock()
+	}
 }
 
 func (p *Player) GetChannel() <-chan string {
-	return nil
+	return p.receiveChan
 }
 
 func (p *Player) SendMessage(msg string) error {
-	return nil
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.currentMapId == 0 || p.pointerToMap == nil {
+		return errors.New("player is not in any map")
+	}
+	formattedMsg := fmt.Sprintf("%s says: %s", p.GetName(), msg)
+
+	rawMsg := Message{
+		sender: p.lowerName,
+		text:   formattedMsg,
+	}
+	select {
+	case p.pointerToMap.mapChatChan <- rawMsg:
+		return nil
+	default:
+		return errors.New("failed to send message")
+	}
+}
+
+func formatName(name string) string {
+	name = strings.TrimSpace(name)
+	if len(name) == 0 {
+		return ""
+	}
+	return strings.ToUpper(name[:1]) + strings.ToLower(name[1:])
 }
 
 func (p *Player) GetName() string {
-	return ""
+	return p.name
 }

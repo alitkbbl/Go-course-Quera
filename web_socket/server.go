1258b93eae98931f3b76f8bc09c4d363cd79afa0
alitkbbl <alitaghi8305@gmail.com>
2025-09-14 17:27:31 +0330

    develope server in web socket

--- web_socket/server.go
+++ web_socket/server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,10 +12,30 @@ var addr = "localhost:8080"
 
 var upgrader = websocket.Upgrader{}
 
-// TODO: add a websocket handler by `upgrader`
-
+func wsHello(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	connection, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade error:", err)
+		return
+	}
+	defer connection.Close()
+	for {
+		messageType, message, err := connection.ReadMessage()
+		if err != nil {
+			log.Println("read error:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		response := []byte(fmt.Sprintf("Hello %s", message))
+		err = connection.WriteMessage(messageType, response)
+		if err != nil {
+			log.Println("write error:", err)
+			break
+		}
+	}
+}
 func main() {
-	// TODO: Add handler to receive and send messages
-
+	http.HandleFunc("/ws/hello", wsHello)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
